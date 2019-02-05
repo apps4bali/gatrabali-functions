97d0bbf9f1f4632b32c1cb60d3677eec97044df7
Eka Putra <eka@insight.co>
2019-02-06 00:03:50 +1100

    Added simple function based on pub/sub trigger.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions'
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!")
-});
+export const DataSyncer = functions.pubsub.topic("SyncData").onPublish((message, context) => {
+    console.log(`DataSyncer triggered at: ${context.timestamp}, message: ${JSON.stringify(message.json)}`);
+    // TODO:
+    // - Call miniflux API
+    // - Cleanup the response
+    // - Store to firestore.
+    return true
+})
