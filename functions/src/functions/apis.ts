7d21223c0828f7689702c236dbd8f0bf98563d72
Eka Putra <ekaputra07@gmail.com>
2019-05-26 19:17:31 +1000

    Setup firebase hosting and added cache control to API response.

--- functions/src/functions/apis.ts
+++ functions/src/functions/apis.ts
@@ -1,6 +1,11 @@
 import * as functions from 'firebase-functions'
 import * as db from '../db'
 
+// By default we cache the content on client and server for 1hr.
+function setCacheControl(res, maxAge = 3600, sMaxAge = 3600) {
+    res.set('Cache-Control', `public, max-age=${maxAge}, s-maxage=${sMaxAge}`)
+}
+
 /**
  * API Category news summary
  * Returns summary of news in eacy category/regency.
@@ -31,11 +36,17 @@ export const ApiNews = functions.https.onRequest((req, res) => {
         }
 
         return db.getNewsByCategoryId(categoryId, cursor, limit)
-            .then(result => res.json(result))
+            .then(result => {
+                if (result.length > 0) setCacheControl(res)
+                res.json(result)
+            })
             .catch(_ => res.status(500).send('Internal Server Error'))
     } else {
         return db.getAllNews(cursor, limit)
-            .then(result => res.json(result))
+            .then(result => {
+                if (result.length > 0) setCacheControl(res)
+                res.json(result)
+            })
             .catch(_ => res.status(500).send('Internal Server Error'))
     }
 })
